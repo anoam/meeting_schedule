2057005b225ffd35fdef43366cfe054aa50d80ce
anoam <anoam@yandex.ru>
2018-04-15 21:17:14 +0300

    Time of day & deserializer

--- lib/meeting_deserializer.rb
+++ lib/meeting_deserializer.rb
@@ -6,4 +6,21 @@ require 'invalid_data'
 # Provides {Meeting}s deserialization from string
 class MeetingDeserializer
 
+  MEETING_REGEXP = /(?<title>.+)\s(?<duration>\d+)min/.freeze
+
+  # Restores collection of meetings from string
+  # @param source [String] formatted string that contains serialized meetings
+  #   one per line
+  def restore(source)
+    return [] if source.nil?
+    return [] if source.empty?
+
+    source.split("\n").map do |text_meeting|
+      data = MEETING_REGEXP.match(text_meeting)
+      raise(InvalidData, "invalid format") if data.nil?
+
+      Meeting.new(data[:title], data[:duration].to_i)
+    end
+  end
+
 end
--- server.rb
+++ server.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
+
 require 'sinatra'
+require 'meeting_deserializer'
+require 'invalid_data'
 
 get '/' do
   erb :index
 end
 
 get '/schedule' do
-  'hello'
+  begin
+    deserializer = MeetingDeserializer.new
+    meetings = deserializer.restore(params[:raw_meetings].strip)
+    meetings.join("\n")
+  rescue InvalidData => e
+    e.message
+  end
 end
